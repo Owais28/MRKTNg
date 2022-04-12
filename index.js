5e2b086da2a068c28194300df2ce8d279cbd74c1
Owais Athar <69184167+Owais28@users.noreply.github.com>
2022-04-12 16:34:54 +0530

    khsdfs

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const menuToggleButton = document.querySelector('#menu-button');
 const mobileMenu = document.getElementById('mobile-menu');
-
+const body = document.body;
 menuToggleButton.addEventListener('click', function() {
     mobileMenu.classList.toggle('show');
+})
+
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth >= 475) {
+        mobileMenu.style.display = 'none';
+        menuToggleButton.style.display = 'none';
+        console.log("Screen is greater than 475px");
+        // console.clear()
+    } else {
+        mobileMenu.style.display = 'block';
+        menuToggleButton.style.display = 'block';
+        console.log("Screen is smaller than 475px");
+        // console.clear()
+    }
 })
